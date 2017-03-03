9705daeb996368d1700cb286bfb765b2626f9f8f
Shinji Kumagai <kumagai.s12345@gmail.com>
2017-03-03 17:55:15 +0900

    「連想配列の作成」 課題提出。

---
+++ CampChallenge_PHP/index2-9.php
@@ -0,0 +1,11 @@
+<?php
+$array = array(
+    "1" => "AAA",
+    "hello" => "world",
+    "soeda" => "33",
+    "20" => "20",
+);
+foreach ($array as $key => $value) {
+    # code...
+    echo "key:".$key."value".$value;
+}
