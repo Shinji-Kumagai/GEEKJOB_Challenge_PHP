8c9dd24cd4089762ca93fa13426dae372c2e9851
Shinji Kumagai <kumagai.s12345@gmail.com>
2017-03-06 19:25:49 +0900

    「ファイルの移動」

--- CampChallenge_PHP/index5-2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$access_date = date("Y-m-d");
-setcookie("LastLoginDate", $access_date);
-
-$lastDate = $_COOKIE['LastLoginDate'];
-echo "お帰りなさいご主人さま<br>";
-echo "前回のログインは".$lastDate."ですね";
