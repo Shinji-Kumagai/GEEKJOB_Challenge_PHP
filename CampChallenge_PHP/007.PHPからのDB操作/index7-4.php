56aa67e2b16e6b1030f2a8c2b504be8e92589850
Shinji Kumagai <kumagai.s12345@gmail.com>
2017-03-18 17:45:53 +0900

    「特定のレコードの情報取得と表示」

---
+++ CampChallenge_PHP/007.PHPからのDB操作/index7-4.php
@@ -0,0 +1,30 @@
+<?php
+$db = 'Challenge_db';
+$dsn = 'mysql:host=localhost;dbname='.$db;
+$user = 'kuma';
+$pass = 'pass';
+
+try {
+    $dbh = new PDO($dsn, $user, $pass,
+    array(
+        PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false
+        )
+    );
+
+
+    $catch = $dbh->query('select * from profiles where profilesID = 21');
+    foreach($catch as $a) {?>
+        <p><?php echo 'ID : '.$a['profilesID'].''; ?></p>
+
+        <p><?php echo '  Name : '.$a['name']; ?></p>
+        <p><?php echo '  Tell : '.$a['tell'];  ?></p>
+        <p><?php echo '  Age : '.$a['age'];  ?></p>
+        <p><?php echo '  Birthday : '.$a['birthday'];  ?></p>
+        <?php
+} //foreach
+
+}catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
